6208549cff1e6e9ffc1b9ba08e1dcd1cd11b85e9
jiraxe <oussamajablaoui@gmail.com>
2020-10-21 23:25:11 -0500

    Revert "Revert "filters added""
    This reverts commit 5429f47d40538fbb9b62e1c4a862f65b0c7a633c.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PagesModule } from './pages/pages.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppRoutingModule,
     PagesModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/shop-home/shop-cockpit/catagory-pane/catagory-pane.component.ts
+++ src/app/pages/shop-home/shop-cockpit/catagory-pane/catagory-pane.component.ts
@@ -1,15 +1,90 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from '@angular/material/dialog';
 
+// TODO: add category model instead of string
+
+export interface DialogData {
+  selectedCategories: string[];
+  selectedCategoryList: [{ category: string, checked: boolean}];
+}
 @Component({
   selector: 'app-catagory-pane',
   templateUrl: './catagory-pane.component.html',
   styleUrls: ['./catagory-pane.component.css']
 })
 export class CatagoryPaneComponent implements OnInit {
+  minValue = 0;
+  maxValue = 2000;
+  minMin = 0;
+  maxMin = 2000;
+  minMax = 0;
+  categoryList: string[] = [ 'elecronic', 'laptop' , 'health' , 'sport' ,
+    'elecronic1', 'laptop1' , 'health1' , 'sport1'];
+  selectedCategoryList: string[] = this.categoryList.map(this.initSelectCategoryList);
+  selectedCategories: string[] = new Array();
+  initSelectCategoryList(item): any{
+    return {category: item , checked : false};
+  }
+  minLabel(value: number): any {
 
-  constructor() { }
+    return  value + '$ ';
+  }
+  maxLabel(value: number): any {
+
+    return value + '$';
+
+  }
+  onChangeMin(event: any): void{
+      this.minValue = event.value;
+  }
+  onChangeMax(event: any): void{
+        this.maxValue = event.value;
+  }
+  onRemoveClick(category): void{
+    this.removeItem(this.selectedCategories.indexOf(category));
+  }
+  removeItem(index): void{
+    this.selectedCategoryList.splice(index, 1);
+  }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
+
+  }
+  openDialog(): void {
+    const dialogRef = this.dialog.open(CategoryListDialogComponent, {
+      width: '70%',
+      height: '50%',
+      data : {selectedCategories: this.selectedCategories, selectedCategoryList: this.selectedCategoryList}
+    });
+
+}}
+@Component({
+  templateUrl: 'category-list-dialog.html',
+  styleUrls: ['./category-list-dialog.css']
+})
+export class CategoryListDialogComponent {
+  onChange(item: any): void{
+    item.checked = !item.checked;
+    if (item.checked === true){
+      this.data.selectedCategories.push(item.category);
+    }
+    else {
+      this.removeItem(this.data.selectedCategories.indexOf(item.category));
+    }
+}
+
+  removeItem(index): void{
+    this.data.selectedCategoryList.splice(index, 1);
+  }
+
+  constructor(
+      public dialogRef: MatDialogRef<CategoryListDialogComponent>,
+      @Inject(MAT_DIALOG_DATA) public data: DialogData) {
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 
 }
